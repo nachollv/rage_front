d47d6deef42661eb80bbe5c50f5e10a4ac9d135c
ignacio.llado@idi.es <ignacio.llado@idi.es>
2025-03-07 09:21:21 +0100

    he creado el form y tabla consumos fijos

--- src/app/Protected/consumption/fixed-installation/fixed-installation.component.ts
+++ src/app/Protected/consumption/fixed-installation/fixed-installation.component.ts
@@ -10,7 +10,15 @@ import { DialogComponent } from '../../../dialog/dialog.component';
 })
 export class FixedInstallationComponent {
     fuelForm: FormGroup;
-  
+    /* 'kg CO2/ud', 'g CH4/ud', 'g N2O/ud', 'kg CO2/ud', 'g CH4/ud', 'g N2O/ud', 'kg CO2', 'g CH4', 'g N2O', 'Emisiones Totales (kg CO2e)' */
+    data = [
+      { delegation: 'Central', year: 2024, 'kg CO2/ud-defecto': 25, 'g CH4/ud-defecto': 34.25, 'g N2O/ud-defecto': '23.54', 'kg CO2/ud-otros': 45.345, 'g CH4/ud-otros': 0.00, 'g N2O/ud-otros': 0.00, 'kg CO2': 0.12, 'g CH4':12.23, 'g N2O': 25.21, 'Emisiones Totales (kg CO2e)':154.24, edit: true, delete: true},
+      { delegation: 'Felanitx', year: 2024, 'kg CO2/ud-defecto': 25, 'g CH4/ud-defecto': 34.25, 'g N2O/ud-defecto': '23.54', 'kg CO2/ud-otros': 45.345, 'g CH4/ud-otros': 0.00, 'g N2O/ud-otros': 0.00, 'kg CO2': 0.12, 'g CH4':12.23, 'g N2O': 25.21, 'Emisiones Totales (kg CO2e)':154.24, edit: true, delete: true},
+      { delegation: 'Manacor', year: 2024, 'kg CO2/ud-defecto': 25, 'g CH4/ud-defecto': 34.25, 'g N2O/ud-defecto': '23.54', 'kg CO2/ud-otros': 45.345, 'g CH4/ud-otros': 0.00, 'g N2O/ud-otros': 0.00, 'kg CO2': 0.12, 'g CH4':12.23, 'g N2O': 25.21, 'Emisiones Totales (kg CO2e)':154.24, edit: true, delete: true},  
+      { delegation: 'Calvià', year: 2024, 'kg CO2/ud-defecto': 25, 'g CH4/ud-defecto': 34.25, 'g N2O/ud-defecto': '23.54', 'kg CO2/ud-otros': 45.345, 'g CH4/ud-otros': 0.00, 'g N2O/ud-otros': 0.00, 'kg CO2': 0.12, 'g CH4':12.23, 'g N2O': 25.21, 'Emisiones Totales (kg CO2e)':154.24, edit: true, delete: true},
+      { delegation: 'Andraitx', year: 2024, 'kg CO2/ud-defecto': 25, 'g CH4/ud-defecto': 34.25, 'g N2O/ud-defecto': '23.54', 'kg CO2/ud-otros': 45.345, 'g CH4/ud-otros': 0.00, 'g N2O/ud-otros': 0.00, 'kg CO2': 0.12, 'g CH4':12.23, 'g N2O': 25.21, 'Emisiones Totales (kg CO2e)':154.24, edit: true, delete: true},
+      { delegation: 'Pollença', year: 2024, 'kg CO2/ud-defecto': 25, 'g CH4/ud-defecto': 34.25, 'g N2O/ud-defecto': '23.54', 'kg CO2/ud-otros': 45.345, 'g CH4/ud-otros': 0.00, 'g N2O/ud-otros': 0.00, 'kg CO2': 0.12, 'g CH4':12.23, 'g N2O': 25.21, 'Emisiones Totales (kg CO2e)':154.24, edit: true, delete: true},
+    ];
     constructor(private fb: FormBuilder, public dialog: MatDialog) {
       this.fuelForm = this.fb.group({
         building: ['', Validators.required],
