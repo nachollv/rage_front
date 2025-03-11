4d8e5f751653f86046be036ddac8bc4bdb3fb791
ignacio.llado@idi.es <ignacio.llado@idi.es>
2025-03-11 22:33:20 +0100

    he añadido chart.js y creado un panel de control

--- src/app/control-panel-container/control-panel-container.component.ts
+++ src/app/control-panel-container/control-panel-container.component.ts
@@ -33,12 +33,28 @@ export class ControlPanelContainerComponent implements OnInit {
         datasets: [{
           label: 'Consumption',
           data: [10, 20, 30, 40],
+          backgroundColor: [
+            'rgba(255, 0, 0, 0.8)', // Rojo vivo
+            'rgba(54, 162, 235, 0.8)', // Azul vivo
+            'rgba(255, 206, 86, 0.8)', // Amarillo vivo
+            'rgba(0, 255, 0, 0.8)'  // Verde vivo
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+          borderWidth: 1,
           // this dataset is drawn below
           order: 2
       }, {
           label: 'Objectives',
           data: [10, 15, 19, 17],
           type: 'line',
+          backgroundColor: 'rgba(153, 102, 255, 0.8)', // Púrpura vivo
+          borderColor: 'rgba(153, 102, 255, 1)',
+          borderWidth: 2,
           // this dataset is drawn on top
           order: 1
       }]
