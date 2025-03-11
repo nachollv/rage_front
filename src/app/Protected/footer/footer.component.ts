293a6175ea669ab428293512ed498fab8ca4f412
Ignacio Lladó Vidal <ignacio.llado@idi.es>
2025-03-11 09:50:49 +0100

    he ajustado el header y footer

--- src/app/Protected/footer/footer.component.ts
+++ src/app/Protected/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../../dialog/dialog.component';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(title:string, text: string): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: {
+        title: title,
+        text: text,
+        position: 'center'
+      },
+      width: '400px',
+      height: '300px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('El dialog se cerró');
+    });
+  }
 
 }
