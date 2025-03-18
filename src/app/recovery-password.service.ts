0ce9eaf5f120e209621f58373d3c30a4e47db33d
Ignacio Lladó Vidal <ignacio.llado@idi.es>
2025-03-18 11:24:54 +0100

    sigo con el recovery

--- src/app/recovery-password.service.ts
+++ src/app/recovery-password.service.ts
@@ -1,18 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecoveryPasswordService {
-  private apiUrl = 'https://pre.tramits.idi.es/public/index.php/send-mail';
+  private apiUrl = 'https://pre.tramits.idi.es/public/index.php/recovery';
+
   constructor(private http: HttpClient) {}
 
   sendRecoveryEmail(email: string): Observable<any> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const body = { email };
 
-    return this.http.post(this.apiUrl, body, { headers });
+    return this.http.post(`${this.apiUrl}/password`, body).pipe(
+      catchError((error) => {
+        // Manejar el error y procesar el contenido del mensaje
+        let errorMessage = 'Ocurrió un error desconocido';
+        if (error.error) {
+          if (error.error.error) {
+            errorMessage = error.error.error; // Mensaje de error genérico del servidor
+          }
+          if (error.error.debug) {
+            errorMessage += ` ##Detalles técnicos: ${error.error.debug}`; // Mensaje de depuración del servidor
+          }
+        } else if (error.error instanceof ErrorEvent) {
+          // Error del lado cliente
+          errorMessage = `Error del cliente: ${error.error.message}`;
+        } else {
+          // Error del lado del servidor
+          errorMessage = `Error del servidor: ${error.status} - ${error.message}`;
+        }
+        return throwError(() => new Error(errorMessage)); // Retornar el mensaje de error procesado al componente
+      })
+    );
   }
 }
--- src/app/recovery-password/recovery-password.component.ts
+++ src/app/recovery-password/recovery-password.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { RecoveryPasswordService } from '../recovery-password.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-recovery-password',
@@ -12,10 +13,12 @@ export class RecoveryPasswordComponent implements OnInit {
   recoveryForm!: FormGroup; // El formulario reactivo
   message: string = ''; // Mensaje de retroalimentación
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private recoveryService: RecoveryPasswordService) {}
+  constructor(private fb: FormBuilder, 
+    private http: HttpClient, 
+    private recoveryService: RecoveryPasswordService,
+    private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
-    // Configura el formulario reactivo con validaciones
     this.recoveryForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]]
     });
@@ -31,11 +34,22 @@ export class RecoveryPasswordComponent implements OnInit {
       this.recoveryService.sendRecoveryEmail(email).subscribe({
         next: (response) => {
           this.message = 'Correo enviado exitosamente. Revisa tu bandeja de entrada.';
+          this.showSnackBar(this.message)
         },
         error: (error) => {
-          this.message = 'Hubo un problema al enviar el correo. Por favor, intenta de nuevo en un momento.';
+          this.message = 'Hubo un problema al enviar el correo.<br>'+error+"<br>Por favor, intenta de nuevo en un momento.";
+          this.showSnackBar(error)
         },
       });
     }
+  }
+
+  private showSnackBar(error: string): void {
+    this.snackBar.open(error, 'Close', {
+      duration: 15000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'center',
+      panelClass: ['custom-snackbar'],
+    });
   }
 }
