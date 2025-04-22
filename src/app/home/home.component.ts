987003bcdbea8e2ce1646de626d896863765ef10
ignacio.llado@idi.es <ignacio.llado@idi.es>
2025-04-22 20:57:34 +0200

    estoy con home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
 import { AuxHelpingTextsService } from '../services/aux-helping-texts.service';
 import { AuxTextDTO } from '../models/auxText.dto';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -15,33 +16,52 @@ export class HomeComponent {
   homeIntro: string = ''
   title: string = ''
   text: string = ''
+  preferredLang: string = localStorage.getItem('preferredLang') || 'es-ES'
   constructor( public dialog: MatDialog,
     private auxHelpingTextsService: AuxHelpingTextsService
    ) {
-    this.getAuxText(0)
+    switch (this.preferredLang) {
+
+      case 'es':  
+      case 'es-ES': 
+        this.preferredLang = 'es'
+        break;
+      case 'ca':
+      case 'ca-ES': 
+        this.preferredLang = 'ca'
+        break;
+      case 'en':
+      case 'en-EN':   
+        this.preferredLang = 'en'
+        break;
+      default:    
+        this.preferredLang = 'es'
+        break;
    }
+  }
 
   openDialog( id: number ): void {
-    this.auxHelpingTextsService.getAuxTextById(id).subscribe((text: AuxTextDTO | undefined) => {
-      if (text) {
-        this.auxText = text
-        if (localStorage.getItem('preferredLang') === 'es-ES') {
-         this.title = text.titleES
-         this.text = text.sectionTextES
-        } else if (localStorage.getItem('preferredLang') === 'ca-ES') {
-          this.title = text.titleCA
-          this.text = text.sectionTextCA
-        } else if (localStorage.getItem('preferredLang') === 'en-EN') {
-          this.title = text.titleEN
-          this.text = text.sectionTextEN
+    this.auxHelpingTextsService.getAuxTextById(id)
+    .pipe(
+      filter((text: AuxTextDTO | undefined) => text !== undefined) // Filtra solo los valores definidos
+    )
+    .subscribe((text: AuxTextDTO | undefined) => {
+    
+      this.auxText = text
+        if (this.preferredLang === 'es') {
+         this.title = text!.titleES
+         this.text = text!.sectionTextES
+        } else if (this.preferredLang === 'ca') {
+          this.title = text!.titleCA
+          this.text = text!.sectionTextCA
+        } else if (this.preferredLang === 'en') {
+          this.title = text!.titleEN
+          this.text = text!.sectionTextEN
         } else {
           console.error('Idioma no soportado....')
         }
-      } else {
-        console.error('Texto auxiliar no encontrado');
-      }
     });
-
+    console.log(this.title, this.text)
     const dialogRef = this.dialog.open(DialogComponent, {
       data: {
         title: this.title,
