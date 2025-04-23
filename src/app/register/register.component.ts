cf1c67e2ee53e3f809411430994265dcef685483
Ignacio Lladó Vidal <ignacio.llado@idi.es>
2025-04-23 08:59:30 +0200

    estoy con register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl, } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { passwordMatchValidator } from '../custom-validators/custom.validator';
 import { OrganizacionService } from '../services/organizacion.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -10,64 +10,77 @@ import { Router } from '@angular/router';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
-  registerForm!: FormGroup; /* ! operador de aserción no nulo (!) para indicar que registerForm será inicializado antes de ser utilizado. */
-  submitted:boolean = false;
-  constructor(private fb: FormBuilder, 
-    private registerService: OrganizacionService, 
-    private snackBar: MatSnackBar,
-    private router: Router) {}
 
-  ngOnInit(): void {
-    this.registerForm = this.fb.group({
+export class RegisterComponent  {
+  registerForm!: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar, private registerService: OrganizacionService, private router: Router) {
+    this.registerForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required,  
-        Validators.minLength(6),
-        this.hasUppercase,
-        this.hasNumber,
-        this.hasSpecialCharacter,]],
-      retypePassword: ['', [Validators.required, Validators.minLength(6), passwordMatchValidator]],
+      password: ['', [ Validators.required, Validators.minLength(6), this.passwordValidator
+      ]],
+      retypePassword: ['', Validators.required],
       cif: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
-      companyName: ['', [Validators.required]],
-      nombre: [''],
-      rol: ['Admin'],
-      zipCode: ['', [Validators.minLength(5), Validators.maxLength(5)]]
+      companyName: ['', Validators.required],
+      zipCode: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]]
+    }, { validators: this.passwordsMatchValidator });
+
+    this.registerForm.get('retypePassword')?.valueChanges.subscribe(() => {
+      const password = this.registerForm.get('password')?.value;
+      const retypePassword = this.registerForm.get('retypePassword')?.value;
+    
+      if (password !== retypePassword) {
+        this.registerForm.setErrors({ passwordsMismatch: true });
+      } else {
+        this.registerForm.setErrors(null);
+      }
     });
   }
 
-   // Custom validator to check if the password contains at least one uppercase letter
-   hasUppercase(control: AbstractControl) {
-    const value = control.value;
-    if (value && !/[A-Z]/.test(value)) {
-        return { uppercase: true };
-    }
-    return null;
-}
+  private passwordsMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const retypePassword = group.get('retypePassword')?.value;
+  
+    console.log('Password:', password); // Para depuración
+    console.log('RetypePassword:', retypePassword); // Para depuración
 
-// Custom validator to check if the password contains at least one number
-hasNumber(control: AbstractControl) {
-    const value = control.value;
-    if (value && !/\d/.test(value)) {
-        return { number: true };
+    if (password === retypePassword) {
+      return null; // No hay error
+    } else {
+      return { passwordsMismatch: true }; // Error encontrado
     }
-    return null;
-}
+  }
+   
 
-// Custom validator to check if the password contains at least one special character
-hasSpecialCharacter(control: AbstractControl) {
-    const value = control.value;
-    if (value && !/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
-        return { specialCharacter: true };
+  private passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.value;
+    const errors: ValidationErrors = {};
+    
+    if (!/[A-Z]/.test(password)) {
+      errors['uppercase'] = true;
     }
-    return null;
-}
+    if (!/[a-z]/.test(password)) {
+      errors['lowercase'] = true;
+    }
+    if (!/\d/.test(password)) {
+      errors['number'] = true;
+    }
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      errors['specialCharacter'] = true;
+    }
+    console.log('Password errors:', errors); // Para verificar el resultado
+    return Object.keys(errors).length ? errors : null;
+  }
+  
 
 onSubmit(): void {
-  this.submitted = true;
 
   if (this.registerForm.valid) {
     const formData = this.registerForm.value;
     formData.nombre = this.registerForm.get('companyName')?.value;
+    formData.rol = 'Admin'
+    formData.daysPasswordDuration = 60
     // Llama al servicio para registrar la organización
     this.registerService.crearOrganizacion(formData).subscribe(
       (response) => {
