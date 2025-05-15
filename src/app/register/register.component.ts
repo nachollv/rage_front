2a44f4a0c7ea7b86e1c114db87b97aed445a9a87
Ignacio Lladó Vidal <ignacio.llado@idi.es>
2025-05-15 11:57:05 +0200

    añadiendo atributos aria y traduciendo

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,7 @@ import { passwordMatchValidator } from '../custom-validators/custom.validator';
 import { OrganizacionService } from '../services/organizacion.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { SignUpMailService } from '../sign-up-mail.service';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +15,10 @@ import { Router } from '@angular/router';
 export class RegisterComponent  {
   registerForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar, private registerService: OrganizacionService, private router: Router, private cdr: ChangeDetectorRef) {
+  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar, 
+    private registerService: OrganizacionService, 
+    private signUpMailService: SignUpMailService,
+    private router: Router, private cdr: ChangeDetectorRef) {
     this.registerForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6), this.passwordValidator]],
@@ -27,13 +31,6 @@ export class RegisterComponent  {
     this.registerForm.get('retypePassword')?.valueChanges.subscribe(() => {
       this.registerForm.updateValueAndValidity({ onlySelf: false });
     });
-
-/*     this.registerForm.valueChanges.subscribe(() => {
-      this.registerForm.markAsTouched();
-      this.registerForm.updateValueAndValidity();
-    }); */
-    
-    
   }
 
   ngOnInit() {
@@ -72,7 +69,17 @@ export class RegisterComponent  {
       this.registerService.crearOrganizacion(formData).subscribe(
         (response) => {
           this.showSnackBar('Su empresa ha sido registrada correctamente.');
+          const email = this.registerForm.value.email;
+      this.signUpMailService.sendSignUpEmail(email).subscribe({
+        next: (response) => {
+          this.showSnackBar('Correo enviado exitosamente. Revisa tu bandeja de entrada.');
           this.router.navigate(['/login']);
+        },
+        error: (error) => {
+          this.showSnackBar('Hubo un problema al enviar el correo.<br>' + error + '<br>Por favor, intenta de nuevo en un momento.');
+        },
+      })
+          
         },
         (error) => {
           this.showSnackBar('Error en el registro: ' + error.messages);
---
+++ src/app/sign-up-mail.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SignUpMailService {
+  private apiUrl = 'https://tramits.idi.es/public/assets/utils/enviaCorreoElectronicoSignUpRAGE.php';
+
+  constructor(private http: HttpClient) {}
+
+  sendSignUpEmail(email: any): Observable<any> {
+    return this.http
+    .get<any>(`${this.apiUrl}?${email}`).pipe(
+      map(response => {
+        console.log ("response.status ", response.status)
+        if (response.status === 'success') {
+          console.log ("Email sent", response.message)
+        } else {
+          console.error("Error sending mail: ", response.message)
+        }
+        return response
+      }),
+      catchError((error) => {
+        let errorMessage = 'Ocurrió un error desconocido';
+        if (error.error) {
+          if (error.error.error) {
+            errorMessage = error.error.error; // Mensaje de error genérico del servidor
+          }
+          if (error.error.debug) {
+            errorMessage += ` ##Detalles técnicos: ${error.error.debug}`; // Mensaje de depuración del servidor
+          }
+        } else if (error.error instanceof ErrorEvent) {
+          // Error del lado cliente
+          errorMessage = `Error del cliente: ${error.error.message}`;
+        } else {
+          // Error del lado del servidor
+          errorMessage = `Error del servidor: ${error.status} - ${error.message}`;
+        }
+        return throwError(() => new Error(errorMessage)); // Retornar el mensaje de error procesado al componente
+      })
+    );
+  }
+}
